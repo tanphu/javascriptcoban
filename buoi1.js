d3444e2b7dcdbb7ddcd85ac781b8776c0c0f810c
Nguyễn Tấn Phú <31039392+tanphu@users.noreply.github.com>
2019-10-06 09:23:57 +0700

    finish buoi1.js

--- buoi1.js
+++ buoi1.js
@@ -1,4 +1,12 @@
 var a = "Nguyen tan phu"
 a =10;
 console.log(a);
+//object dưới dạng key
+//mảng là index
 
+var animal = [
+    {name : 'cat', age : 2},
+    {name : 'Mouse', age : 3},
+    {name : 'Dog' , age : 4},
+]
+console.log(animal);
